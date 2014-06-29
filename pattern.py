029b9d763112ff86d43ca95d43fa7124f5fc1eaf
PaulPidou <pidou.paul@gmail.com>
2014-06-30 00:43:41 +0200

    Add new patterns

--- pattern.py
+++ pattern.py
@@ -79,24 +79,106 @@ class Stable(Pattern):
             self.width, self.height = 2,4                        #                      
             self.fixes = [(0,0),(1,0),(0,3),(1,3)]              #
             self.possibilities = [[(1,1),(0,2)],[(0,1),(1,2)]]  ##
-
         else: # horizontal                              ## # Horizontal default                                     
             self.width, self.height = 4,2               # ##
             self.fixes = [(0,0),(0,1),(3,0),(3,1)]
             self.possibilities = [[(1,0),(2,1)],[(1,1),(2,0)]]
-
         self.createPattern(ch, False)
         return self.pattern
 
     def createBarge(self, orient=0):
-        self.resetPattern()
-        self.width = self.height = 4
-        if orient == 1: # from left to right
-            self.fixes = [(1,0),(0,1),(2,1),(1,2),(3,2),(2,3)]
-        else:
+        self.resetPattern()                                         #  Default
+        self.width = self.height = 4                               # #
+        if orient == 1: # from left to right                      # #
+            self.fixes = [(1,0),(0,1),(2,1),(1,2),(3,2),(2,3)]     #
+        else: # from right to left
             self.fixes = [(2,0),(1,1),(3,1),(0,2),(2,2),(1,3)]
         self.createPattern()
         return self.pattern
             
+    def createAircraftCarrier(self, ch=0, orient=0):
+        self.resetPattern()
+        if orient == 1: # vertical                                                          ## Vertical default
+            self.width, self.height = 3,4                                                   #
+            self.fixes = [(1,0),(1,3)]                                                        #
+            self.possibilities = [[(0,0),(0,1),(2,2),(2,3)],[(2,0),(2,1),(0,2),(0,3)]]       ##
+        else : # horizontal
+            self.width, self.height = 4,3                                                     ## Horizontal default
+            self.fixes = [(0,1),(3,1)]                                                      #  #
+            self.possibilities = [[(0,2),(1,2),(2,0),(3,0)],[(0,0),(1,0),(2,2),(3,2)]]      ##
+        self.createPattern(ch, False)
+        return self.pattern
             
-        
+    def createHive(self, orient=0):
+        self.resetPattern()
+        if orient == 1: # vertical                                  #  Vertical default
+            self.width, self.height = 3,4                          # #
+            self.fixes = [(1,0),(0,1),(2,1),(0,2),(2,2),(1,3)]     # #
+        else: # horizontal                                          #
+            self.width, self.height = 4,3
+            self.fixes = [(0,1),(1,0),(1,2),(2,0),(2,2),(3,1)]      ##  Horizontal default
+        self.createPattern()                                       #  #
+        return self.pattern                                         ##
+
+    def createBreadLoad(self, ch=0, orient=0):
+        self.resetPattern()
+        self.width = self.height = 4
+        if orient == 1: # low base                                          #   Low base default
+            self.fixes = [(0,2),(1,3),(2,3),(3,2)]                         # #
+            self.possibilities = [[(0,1),(1,0),(2,1)],[(1,1),(2,0),(3,1)]] #  #
+        else: # high base                                                   ##      ##  High base default
+            self.fixes = [(1,0),(2,0),(0,1),(3,1)]                                 #  #
+            self.possibilities = [[(0,2),(1,3),(2,2)],[(1,2),(2,3),(3,2)]]         # #
+        self.createPattern(ch, False)                                               #
+        return self.pattern
+
+    def createHook(self, ch=0, orient=0):       ##   Low head default       ## High head default
+        self.resetPattern()                      #                         # #
+        self.width = self.height = 4             # #                       #
+        if orient == 1: # high head               ##                      ##
+            self.possibilities = [[(2,0),(3,0),(1,1),(3,1),(1,2),(0,3),(1,3)],[(0,0),(1,0),(0,1),(2,1),(2,2),(2,3),(3,3)]]
+        else: # low head
+            self.possibilities = [[(0,0),(1,0),(1,1),(1,2),(3,2),(2,3),(3,3)],[(2,0),(3,0),(2,1),(0,2),(2,2),(0,3),(1,3)]]
+        self.createPattern(ch, False)
+        return self.pattern
+
+    def createCanoe(self, ch=0, orient=0):
+        self.resetPattern()                                         ##     From left to right
+        self.width = self.height = 5                                #      Default
+        if orient == 1: # from right to left                         #
+            self.fixes = [(3,0),(4,0),(4,1),(0,3),(0,4),(1,4)]        # #
+            self.possibilities = [[(3,2),(2,3)],[(2,1),(1,2)]]         ##
+        else: # from left to right
+            self.fixes = [(0,0),(1,0),(0,1),(4,3),(3,4),(4,4)]
+            self.possibilities = [[(1,2),(2,3)],[(2,1),(3,2)]]
+        self.createPattern(ch, False)
+        return self.pattern
+
+    def createLongBarge(self, orient=0):
+        self.resetPattern()                                                         #   Default
+        self.width = self.height = 5                                               # #
+        if orient == 1: # from left to right                                      # #
+            self.fixes = [(1,0),(0,1),(2,1),(1,2),(3,2),(2,3),(4,3),(3,4)]       # #
+        else: # from right to left                                                #
+            self.fixes = [(3,0),(2,1),(4,1),(1,2),(3,2),(0,3),(2,3),(1,4)]
+        self.createPattern()
+        return self.pattern
+
+    def createLongShip(self, orient=0):
+        self.resetPattern()                                                     ## Default
+        self.width = self.height = 4                                           # #
+        if orient == 1: # from left to right                                  # #
+            self.fixes = [(0,0),(1,0),(0,1),(2,1),(1,2),(3,2),(2,3),(3,3)]    ##
+        else: # from right to left
+            self.fixes = [(2,0),(3,0),(1,1),(3,1),(0,2),(2,2),(0,3),(1,3)]
+        self.createPattern()
+        return self.pattern
+
+    def createPond(self):
+        self.resetPattern()                                                 ##
+        self.width = self.height = 4                                       #  #
+        self.fixes = [(1,0),(2,0),(0,1),(3,1),(0,2),(3,2),(1,3),(2,3)]     #  #
+        self.createPattern()                                                ##
+        return self.pattern
+
+    
